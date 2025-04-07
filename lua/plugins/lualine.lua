85b8366b52262523e8d083e0f33d71791de71797
Nyle <nylewagjiani@gmail.com>
2025-04-07 15:08:01 +0100

    made the lualine more round

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,25 +1,31 @@
 return {
-	{
-		"hoob3rt/lualine.nvim",
-		dependencies = { "kyazdani42/nvim-web-devicons" },
-		config = function()
-			-- Lua configuration for lualine with catppuccin theme
-			require("lualine").setup({
-				options = {
-					theme = "catppuccin",
-					section_separators = { "", "" },
-					component_separators = { "", "" },
-				},
-				sections = {
-					lualine_a = { "mode" },
-					lualine_b = { "branch", "diff", "diagnostics" },
-					lualine_c = { "filename" },
-					lualine_x = { "encoding", "filetype" },
-					lualine_y = { "progress" },
-					lualine_z = { "location" },
-				},
-				extensions = { "fugitive" },
-			})
-		end,
-	},
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons", "catppuccin/nvim" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "catppuccin-mocha",
+        component_separators = "",
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { { "mode", right_padding = 2 } },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "%=" },
+        lualine_x = {},
+        lualine_y = { "filetype", "progress" },
+        lualine_z = { { "location", left_padding = 2 } },
+      },
+      inactive_sections = {
+        lualine_a = { "filename" },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { "location" },
+      },
+      tabline = {},
+      extensions = {},
+    })
+  end,
 }
