1e4fc7a2c1839ce8cf0a4e297b541f27a83c63d6
nyle <NyleWagjiani@Gmail.com>
2025-04-15 21:13:10 +0100

    fixed some things

--- init.lua
+++ init.lua
@@ -11,7 +11,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -29,3 +29,4 @@ vim.g.maplocalleader = "\\"
 -- Setup lazy.nvim
 require("lazy").setup("plugins")
 
+vim.keymap.set("n", "<leader>rn", ":set relativenumber!<CR>")
