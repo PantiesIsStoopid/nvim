8bed8fcba35ffc8bd9ddb958a8f5952b09284f02
Nyle <nylewagjiani@gmail.com>
2025-04-16 10:29:06 +0100

    added alpha and remove arrow keys

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,3 +4,7 @@ vim.api.nvim_set_keymap("n", "<leader>vs", ":vsplit<CR>", { noremap = true })
 -- Open horizontal split with :sp or <leader>hs
 vim.api.nvim_set_keymap("n", "<leader>hs", ":split<CR>", { noremap = true })
 
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
