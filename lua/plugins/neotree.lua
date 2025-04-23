73698a969189d087f9d472e49b15dc4bfddf72cb
Nyle <nylewagjiani@gmail.com>
2025-04-23 18:26:45 +0100

    make neotree better and added auto save to neovim

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,16 +1,28 @@
 return {
-  
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v2.x",
-    dependencies = { "MunifTanjim/nui.nvim" },
+    dependencies = {
+      "MunifTanjim/nui.nvim"
+    },
     config = function()
       require("neo-tree").setup({
+        default_component_configs = {
+          icon = {
+            folder_closed = "",
+            folder_open = "",
+            folder_empty = "",
+            file = "",
+          },
+        },
         close_if_last_window = true,
         popup_border_style = "rounded",
         enable_git_status = true,
-        enable_diagnostics = true
+        enable_diagnostics = true,
       })
+
       vim.keymap.set("n", "<C-b>", "<cmd>Neotree toggle<cr>")
-    end
-  },}
+    end,
+  }
+}
+
