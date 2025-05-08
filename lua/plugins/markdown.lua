e36c15e8328eb3a1941812be229b5f56e4c8b9c6
Nyle <nylewagjiani@gmail.com>
2025-05-08 14:24:38 +0100

    added better lsp and markdown preview

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,12 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  build = "cd app && npm install",
+  ft = { "markdown" },
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  config = function()
+    vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreview<CR>", { desc = "Markdown Preview" })
+  end,
+}
+
