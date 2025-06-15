d5215a85345eb87ca30fbe981149a50654be6d42
Nyle Wagjiani <NyleWagjiani@Gmail.Com>
2025-06-15 13:18:14 +0100

    made neotree load earlyier

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,36 +1,36 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v2.x",
-    dependencies = {
-      "MunifTanjim/nui.nvim"
-    },
-    config = function()
-      require("neo-tree").setup({
-        default_component_configs = {
-          icon = {
-            folder_closed = "",
-            folder_open = "",
-            folder_empty = "",
-            file = "",
-          },
-        },
-        -- Show hidden files
-        filesystem = {
-          filtered_items = {
-            visible = true, -- This will make hidden files visible
-            hide_dotfiles = false,  -- Do not hide dotfiles
-            hide_gitignore = false, -- Show files ignored by Git
-          },
-        },
-        close_if_last_window = true,
-        popup_border_style = "rounded",
-        enable_git_status = true,
-        enable_diagnostics = true,
-      })
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		priority = 1000,
+		branch = "v2.x",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			require("neo-tree").setup({
+				default_component_configs = {
+					icon = {
+						folder_closed = "",
+						folder_open = "",
+						folder_empty = "",
+						file = "",
+					},
+				},
+				-- Show hidden files
+				filesystem = {
+					filtered_items = {
+						visible = true, -- This will make hidden files visible
+						hide_dotfiles = false, -- Do not hide dotfiles
+						hide_gitignore = false, -- Show files ignored by Git
+					},
+				},
+				close_if_last_window = true,
+				popup_border_style = "rounded",
+				enable_git_status = true,
+				enable_diagnostics = true,
+			})
 
-      vim.keymap.set("n", "<C-b>", "<cmd>Neotree toggle<cr>", { desc = "Toggle Neo-tree file explorer" })
-    end,
-  }
+			vim.keymap.set("n", "<C-b>", "<cmd>Neotree toggle<cr>", { desc = "Toggle Neo-tree file explorer" })
+		end,
+	},
 }
-
