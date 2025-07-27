1addf3e4737352cac8966d9d6210bb19cf3edf7c
nyle wagjiani <nylewagjiani@gmail.com>
2025-07-27 13:29:36 +0100

    changes to git ignore and added error lens style diagnostics messages

--- init.lua
+++ init.lua
@@ -17,3 +17,15 @@ require("config.lazy", {
 
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = "●",
+    source = "if_many",
+    spacing = 1,
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+})
+
