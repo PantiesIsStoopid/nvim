0ff7500ccc01a1733c3fb0b99f3f39b816e168d2
Nyle Wagjiani <NyleWagjiani@Gmail.Com>
2025-08-14 13:52:15 +0100

    Added icons to oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,11 +1,10 @@
 return {
-    "stevearc/oil.nvim",
-    config = function()
-        require("oil").setup()
-        -- Corrected keymap: use <C-b> instead of "cntrl b"
-        vim.keymap.set("n", "<C-b>", "<cmd>Oil<CR>", {
-            noremap = true,
-            silent = true
-        })
-    end
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      use_icons = true
+    })
+    vim.keymap.set("n", "<C-b>", "<cmd>Oil<CR>", { noremap = true, silent = true })
+  end
 }
