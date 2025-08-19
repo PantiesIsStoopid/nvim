2c378a3184c4489b313fbec3e1c8036953dfa298
nyle wagjiani <nylewagjiani@gmail.com>
2025-08-19 11:37:05 +0100

    added readme and fix errors when building markdown preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,8 +1,9 @@
 return {
   'iamcco/markdown-preview.nvim',
-  build = 'cd app && npm install',
-  ft = { 'markdown' },
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  build = 'cd app && yarn install',
   init = function()
     vim.g.mkdp_filetypes = { 'markdown' }
   end,
+  ft = { 'markdown' },
 }
