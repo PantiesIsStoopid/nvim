e84a47033227f47b195895df2b227b0d82ba9792
nyle wagjiani <nylewagjiani@gmail.com>
2025-08-20 11:34:46 +0100

    mini.nvim remove custom keybinds

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -8,13 +8,5 @@ return {
 		require("mini.completion").setup()
 		require("mini.snippets").setup()
 		require("mini.git").setup()
-
-		local Comment = require("mini.comment")
-
-		-- Mini Comment keymaps
-		vim.keymap.set("n", "<Leader>/", Comment.comment_line)
-		vim.keymap.set("v", "<Leader>/", function()
-			Comment.comment_lines()
-		end)
 	end,
 }
