8b65df72ff7aad91707f21d69518001b909bb990
Nyle Wagjiani <NyleWagjiani@Gmail.Com>
2025-08-27 15:45:47 +0100

    better splashscreen

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,46 +1,55 @@
 return {
-	"goolord/alpha-nvim",
-	dependencies = { "nvim-telescope/telescope.nvim" },
-	config = function()
-		local Alpha = require("alpha")
-		local Dashboard = require("alpha.themes.dashboard")
+  "goolord/alpha-nvim",
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  config = function()
+    local Alpha = require("alpha")
+    local Dashboard = require("alpha.themes.dashboard")
 
-		-- Header ASCII
-		Dashboard.section.header.val = {
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                     ]],
-			[[       ████ ██████           █████      ██                     ]],
-			[[      ███████████             █████                             ]],
-			[[      █████████ ███████████████████ ███   ███████████   ]],
-			[[     █████████  ███    █████████████ █████ ██████████████   ]],
-			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-		}
-		-- Buttons
-		Dashboard.section.buttons.val = {
-			Dashboard.button("q", "󰈆  Quit", ":qa<CR>"),
-		}
+    -- Header ASCII
+    Dashboard.section.header.val = {
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                     ]],
+      [[       ████ ██████           █████      ██                     ]],
+      [[      ███████████             █████                             ]],
+      [[      █████████ ███████████████████ ███   ███████████   ]],
+      [[     █████████  ███    █████████████ █████ ██████████████   ]],
+      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+    }
 
-		-- Footer with Lazy.nvim stats
-		Dashboard.section.footer.val = function()
-			local EndTime = vim.loop.hrtime()
-			local TotalTime = math.floor((EndTime - vim.g.startup_time) / 1e6) -- ms
-			local Stats = require("lazy").stats()
-			local Loaded = Stats.loaded
-			local Total = Stats.count
-			return "⚡ Startup Time: " .. TotalTime .. "ms  |  " .. Loaded .. "/" .. Total .. " Plugins Loaded"
-		end
+    -- Buttons
+    Dashboard.section.buttons.val = {
+      Dashboard.button("f", "󰱼  Find File", ":Telescope find_files<CR>"),
+      Dashboard.button("r", "  Recent Files", ":Telescope oldfiles<CR>"),
+      Dashboard.button("s", "  Search Text", ":Telescope live_grep<CR>"),
+      Dashboard.button("c", "  Configuration", ":e $MYVIMRC<CR>"),
+      Dashboard.button("q", "󰈆  Quit", ":qa<CR>"),
+    }
 
-		Alpha.setup(Dashboard.opts)
-	end,
+    -- Footer with Lazy.nvim stats
+    Dashboard.section.footer.val = function()
+      local EndTime = vim.loop.hrtime()
+      local TotalTime = math.floor((EndTime - vim.g.startup_time) / 1e6) -- in ms
+      local Stats = require("lazy").stats()
+      return string.format(
+        "⚡ Startup Time: %dms  |  %d/%d Plugins Loaded",
+        TotalTime,
+        Stats.loaded,
+        Stats.count
+      )
+    end
+
+    -- Setup
+    Alpha.setup(Dashboard.opts)
+  end,
 }
