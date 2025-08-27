d948fdc7db1361981508c22c05b6139a5f5d9bd5
Nyle Wagjiani <NyleWagjiani@Gmail.Com>
2025-08-27 18:17:52 +0100

    add lazy update to alpha splashscreen

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -33,6 +33,7 @@ return {
       Dashboard.button("r", "  Recent Files", ":Telescope oldfiles<CR>"),
       Dashboard.button("s", "  Search Text", ":Telescope live_grep<CR>"),
       Dashboard.button("c", "  Configuration", ":e $MYVIMRC<CR>"),
+      Dashboard.button("l", "󰒲  Lazy Update", ":Lazy update<CR>"),
       Dashboard.button("q", "󰈆  Quit", ":qa<CR>"),
     }
 
