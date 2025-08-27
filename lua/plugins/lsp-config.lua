053c4f43364f9b64f8072737a27afdd4b4f2e2c4
Nyle Wagjiani <NyleWagjiani@Gmail.Com>
2025-08-27 12:48:15 +0100

    fix nullls and mason

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,56 +1,81 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ts_ls", "rust_analyzer", "clangd", "pyright" },
-			})
-		end,
-	},
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		config = function()
-			require("mason-tool-installer").setup({
-				ensure_installed = { "stylua", "prettier", "rustfmt", "clang-format", "black", "isort" },
-				auto_update = true,
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local Capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local Lspconfig = require("lspconfig")
-			local Servers = { "lua_ls", "ts_ls", "rust_analyzer", "clangd", "jsonls", "pyright" }
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "bashls", "pyright", "lua_ls", "ts_ls", "rust_analyzer",
+          "clangd", "html", "cssls", "gopls", "jdtls", "jsonls",
+          "dockerls", "sqlls", "vimls", "yamlls", "lemminx", "zls",
+        },
+      })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    config = function()
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          "stylua", "prettier", "clang-format", "black", "isort",
+          "shfmt", "shellcheck", "markdownlint", "jsonlint",
+          "htmlhint", "hadolint",
+        },
+        auto_update = true,
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local Capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local Lspconfig = require("lspconfig")
 
-			for _, Server in ipairs(Servers) do
-				Lspconfig[Server].setup({ capabilities = Capabilities })
-			end
+      local Servers = {
+        "bashls", "pyright", "lua_ls", "ts_ls", "clangd",
+        "html", "cssls", "gopls", "jdtls", "jsonls",
+        "dockerls", "sqlls", "vimls", "yamlls", "lemminx", "zls",
+      }
 
-			vim.diagnostic.config({
-				virtual_text = { prefix = "●", spacing = 2 },
-				signs = true,
-				update_in_insert = true,
-				underline = true,
-				severity_sort = true,
-			})
+      for _, Server in ipairs(Servers) do
+        Lspconfig[Server].setup({ capabilities = Capabilities })
+      end
 
-			local Keymaps = {
-				{ "n", "K", vim.lsp.buf.hover, "LSP Hover" },
-				{ "n", "gd", vim.lsp.buf.definition, "Go to Definition" },
-				{ "n", "<leader>rn", vim.lsp.buf.rename, "Rename" },
-				{ "n", "<leader>f", vim.lsp.buf.format, "Format Document" },
-				{ "n", "<leader>ca", vim.lsp.buf.code_action, "Code Action" },
-			}
-			for _, M in ipairs(Keymaps) do
-				vim.keymap.set(M[1], M[2], M[3], { desc = M[4] })
-			end
-		end,
-	},
+      -- Rust Analyzer with diagnostics
+      Lspconfig.rust_analyzer.setup({
+        capabilities = Capabilities,
+        settings = {
+          ["rust-analyzer"] = {
+            diagnostics = { enable = true },
+            cargo = { allFeatures = true },
+checkOnSave = true,
+          },
+        },
+      })
+
+      vim.diagnostic.config({
+        virtual_text = { prefix = "●", spacing = 2 },
+        signs = true,
+        update_in_insert = true,
+        underline = true,
+        severity_sort = true,
+      })
+
+      local Keymaps = {
+        { "n", "K", vim.lsp.buf.hover, "LSP Hover" },
+        { "n", "gd", vim.lsp.buf.definition, "Go to Definition" },
+        { "n", "<leader>rn", vim.lsp.buf.rename, "Rename" },
+        { "n", "<leader>f", vim.lsp.buf.format, "Format Document" },
+        { "n", "<leader>ca", vim.lsp.buf.code_action, "Code Action" },
+      }
+      for _, M in ipairs(Keymaps) do
+        vim.keymap.set(M[1], M[2], M[3], { desc = M[4] })
+      end
+    end,
+  },
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,31 +1,31 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local nonels = require("null-ls")
-    local FormatGroup = vim.api.nvim_create_augroup("FormatOnSave", { clear = true })
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
 
-    nonels.setup({
-      sources = {
-        nonels.builtins.formatting.stylua,
-        nonels.builtins.formatting.prettier,
-        nonels.builtins.formatting.rustfmt,
-        nonels.builtins.formatting.clang_format,
-        nonels.builtins.formatting.black,
-        nonels.builtins.formatting.isort,
-      },
-
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = FormatGroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = FormatGroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ bufnr = bufnr })
-            end,
-          })
-        end
-      end,
-    })
-  end,
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.clang_format,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+				null_ls.builtins.formatting.shfmt,
+				null_ls.builtins.formatting.sql_formatter,
+			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ async = true })
+						end,
+					})
+				end
+			end,
+		})
+	end,
 }
