5588747eb10df1adcdf6a17f751e9c601cbf9b44
nyle wagjiani <nylewagjiani@gmail.com>
2025-09-07 16:26:31 +0100

    formatting

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,47 +1,47 @@
 return {
-  "saghen/blink.cmp",
-  dependencies = { "rafamadriz/friendly-snippets" },
-  version = "1.*",
+	"saghen/blink.cmp",
+	dependencies = { "rafamadriz/friendly-snippets" },
+	version = "1.*",
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    keymap = { preset = "super-tab" },
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		keymap = { preset = "super-tab" },
 
-    appearance = {
-      nerd_font_variant = "mono",
-    },
+		appearance = {
+			nerd_font_variant = "mono",
+		},
 
-    completion = {
-      documentation = { auto_show = true, window = { border = "rounded" } },
-      menu = {
-        border = "rounded",
-        winblend = 10, -- slight transparency
-      },
-    },
+		completion = {
+			documentation = { auto_show = true, window = { border = "rounded" } },
+			menu = {
+				border = "rounded",
+				winblend = 10, -- slight transparency
+			},
+		},
 
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-    },
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
 
-    -- Better suggestion ranking
-    matching = {
-      disallow_fuzzy_matching = false,
-      disallow_partial_matching = false,
-      disallow_prefix_unmatching = false,
-    },
-    sorting = {
-      comparators = {
-        "score", -- relevance score
-        "locality", -- prefer nearby words
-        "recency", -- recently used
-        "kind", -- function/variable/etc
-        "length", -- shorter matches first
-      },
-    },
+		-- Better suggestion ranking
+		matching = {
+			disallow_fuzzy_matching = false,
+			disallow_partial_matching = false,
+			disallow_prefix_unmatching = false,
+		},
+		sorting = {
+			comparators = {
+				"score", -- relevance score
+				"locality", -- prefer nearby words
+				"recency", -- recently used
+				"kind", -- function/variable/etc
+				"length", -- shorter matches first
+			},
+		},
 
-    fuzzy = { implementation = "prefer_rust_with_warning" },
-  },
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+	},
 
-  opts_extend = { "sources.default" },
+	opts_extend = { "sources.default" },
 }
