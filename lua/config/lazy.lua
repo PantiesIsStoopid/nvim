e28c56451e495d88616d3f3e42ae224e7e6e799f
nyle wagjiani <nylewagjiani@gmail.com>
2025-09-28 16:00:03 +0100

    better lsp better format better completions

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,17 +1,17 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 ---@diagnostic disable-next-line: undefined-field
 if not vim.loop.fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,19 +21,19 @@ vim.g.maplocalleader = "\\"
 
 -- Lazy.nvim setup
 require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-    {
-      "catppuccin/nvim",
-      name = "catppuccin",
-      priority = 1000,
-      config = function()
-        vim.cmd.colorscheme "catppuccin"
-      end,
-    }
-  },
+	spec = {
+		{ import = "plugins" },
+		{
+			"catppuccin/nvim",
+			name = "catppuccin",
+			priority = 1000,
+			config = function()
+				vim.cmd.colorscheme("catppuccin")
+			end,
+		},
+	},
 
-  checker = { enabled = true },
-  install = { missing = true },
-  defaults = { version = "*" },
+	checker = { enabled = true },
+	install = { missing = true },
+	defaults = { version = "*" },
 })
--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,30 +1,40 @@
 return {
-  "saghen/blink.cmp",
-  dependencies = { "rafamadriz/friendly-snippets" },
-  version = "1.*",
-
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    keymap = { preset = "super-tab" },
-
-    appearance = {
-      nerd_font_variant = "mono",
-    },
-
-    completion = {
-      documentation = { auto_show = true, window = { border = "rounded" } },
-      menu = {
-        border = "rounded",
-      },
-    },
-
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-    },
-
-    fuzzy = { implementation = "prefer_rust_with_warning" },
-  },
-
-  opts_extend = { "sources.default" },
+	"saghen/blink.cmp",
+	event = "VimEnter",
+	version = "1.*",
+	dependencies = {
+		{
+			"L3MON4D3/LuaSnip",
+			version = "2.*",
+			build = (function()
+				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+					return
+				end
+				return "make install_jsregexp"
+			end)(),
+			dependencies = {},
+			opts = {},
+		},
+		"folke/lazydev.nvim",
+	},
+	opts = {
+		keymap = {
+			preset = "default",
+		},
+		appearance = {
+			nerd_font_variant = "mono",
+		},
+		completion = {
+			documentation = { auto_show = false, auto_show_delay_ms = 500 },
+		},
+		sources = {
+			default = { "lsp", "path", "snippets", "lazydev" },
+			providers = {
+				lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
+			},
+		},
+		snippets = { preset = "luasnip" },
+		fuzzy = { implementation = "lua" },
+		signature = { enabled = true },
+	},
 }
