d3446e1188edd90782581583cde2c306208f3fff
nyle wagjiani <nylewagjiani@gmail.com>
2025-10-04 16:30:15 +0100

    not lazy load mason

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,19 +12,17 @@ return {
 	{
 		-- Main LSP Configuration
 		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
 			-- Automatically install LSPs and related tools to stdpath for Neovim
 			-- Mason must be loaded before its dependents so we need to set it up here.
 			-- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-			{ "mason-org/mason.nvim", event = "VeryLazy", opts = {} },
-			{ "mason-org/mason-lspconfig.nvim", event = "VeryLazy" },
+			{ "mason-org/mason.nvim", opts = {} },
+			{ "mason-org/mason-lspconfig.nvim" },
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 
 			-- Useful status updates for LSP.
 			{
 				"j-hui/fidget.nvim",
-				event = "LspAttach",
 				config = true,
 			},
 		},
