8a879db6d55ab2921ee818b19c3d8099856a42df
nyle wagjiani <nylewagjiani@gmail.com>
2025-10-04 16:42:48 +0100

    made oil in floating window as it looks better

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,12 +1,23 @@
 return {
-	"stevearc/oil.nvim",
-	opts = {
-		view_options = {
-			show_hidden = true,
-		},
-		columns = {
-			"icon", -- Enable the icon column
-		},
+	{
+		"JezerM/oil-lsp-diagnostics.nvim",
+		dependencies = { "stevearc/oil.nvim" },
+		opts = {},
 	},
-	keys = { { "-", "<CMD>Oil<CR>", desc = "Open parent directory" } },
+	{
+		"stevearc/oil.nvim",
+		opts = {
+			view_options = {
+				show_hidden = true,
+			},
+			columns = {
+				"icon", -- Enable the icon column
+			},
+			delete_to_trash = true,
+			float = { padding = 2, max_width = 80, max_height = 20, border = "rounded" },
+		},
+		vim.keymap.set("n", "-", function()
+			require("oil").toggle_float()
+		end, { desc = "Open Oil floating window in parent directory" }),
+	},
 }
